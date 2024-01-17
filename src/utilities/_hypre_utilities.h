4e0e0ab0b8df646bc64db78c3274cc2e69cd59b3
Oleg Ryabkov <oleg.ryabkov.87@gmail.com>
2024-01-17 15:42:12 +0300

    commented current version of OLD_CUDA macro added by Nikolay (currently it always defined which does not make sense), added help file to build on oleg's notebook.

--- src/utilities/_hypre_utilities.h
+++ src/utilities/_hypre_utilities.h
@@ -20,9 +20,9 @@ extern "C" {
  *
  * SPDX-License-Identifier: (Apache-2.0 OR MIT)
  ******************************************************************************/
-#ifndef OLD_CUDA
+/*#ifndef OLD_CUDA
 #define OLD_CUDA //used for old architecture with sm<6.0
-#endif
+#endif*/
 
 
 #ifndef hypre_STATE_HEADER
