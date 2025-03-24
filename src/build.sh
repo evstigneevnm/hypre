370a8e8d0f65196947b95303e42961d2973bf4a3
Oleg Ryabkov <oleg.ryabkov.87@gmail.com>
2025-03-24 17:00:48 +0300

    added build.sh script to automate hypre build and configure (to use in upper solvers Makefile) + minor comment in make.txt

---
+++ src/build.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -x #echo on
+MPI_ROOT_PATH=$1
+CUDA_ROOT_PATH=$2
+CUDA_ARCH_LIST=$3
+echo "CUDA_ARCH_LIST=$CUDA_ARCH_LIST"
+if [[ "$4" == "float" ]]
+then
+    echo "Using SINGLE PRECISION"
+    HYPRE_PRECISION_FLAG=--enable-single
+fi
+make clean
+./configure --with-MPI --enable-gpu-aware-mpi --with-MPI-include=$MPI_ROOT_PATH/include --with-MPI-lib-dirs=$MPI_ROOT_PATH/lib --enable-cuda-streams --with-cuda-home=$CUDA_ROOT_PATH --with-gpu-arch="$CUDA_ARCH_LIST" --enable-unified-memory $HYPRE_PRECISION_FLAG --enable-debug
+make -j
+cd test
+make ij -j
+cd ..
