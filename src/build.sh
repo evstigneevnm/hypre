c4ef62ed535e701cd722ef297b1cf9b4b5c2c902
Oleg Ryabkov <oleg.ryabkov.87@gmail.com>
2025-03-25 11:17:54 +0300

    in build.sh added parameter to control debug, added --with-openmp unconditionally

--- src/build.sh
+++ src/build.sh
@@ -9,8 +9,13 @@ then
     echo "Using SINGLE PRECISION"
     HYPRE_PRECISION_FLAG=--enable-single
 fi
+if [[ "$5" == "debug" ]]
+then
+    echo "Using DEBUG VERSION"
+    HYPRE_DEBUG_FLAG=--enable-debug
+fi
 make clean
-./configure --with-MPI --enable-gpu-aware-mpi --with-MPI-include=$MPI_ROOT_PATH/include --with-MPI-lib-dirs=$MPI_ROOT_PATH/lib --enable-cuda-streams --with-cuda-home=$CUDA_ROOT_PATH --with-gpu-arch="$CUDA_ARCH_LIST" --enable-unified-memory $HYPRE_PRECISION_FLAG --enable-debug
+./configure --with-MPI --with-openmp --enable-gpu-aware-mpi --with-MPI-include=$MPI_ROOT_PATH/include --with-MPI-lib-dirs=$MPI_ROOT_PATH/lib --enable-cuda-streams --with-cuda-home=$CUDA_ROOT_PATH --with-gpu-arch="$CUDA_ARCH_LIST" --enable-unified-memory $HYPRE_PRECISION_FLAG $HYPRE_DEBUG_FLAG
 make -j
 cd test
 make ij -j
