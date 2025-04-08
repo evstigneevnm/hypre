3c6dbc0b69831da1a1c0495beeaa8442118acf78
evstigneevnm <evstigneevnm@gmail.com>
2025-04-08 22:47:53 +0300

    gpu aware mpi optional fix

--- src/build.sh
+++ src/build.sh
@@ -3,6 +3,7 @@ set -x #echo on
 MPI_ROOT_PATH=$1
 CUDA_ROOT_PATH=$2
 CUDA_ARCH_LIST=$3
+HYPRE_ENABLE_GPU_AWARE_MPI=--enable-gpu-aware-mpi 
 echo "CUDA_ARCH_LIST=$CUDA_ARCH_LIST"
 if [[ "$4" == "float" ]]
 then
@@ -14,8 +15,15 @@ then
     echo "Using DEBUG VERSION"
     HYPRE_DEBUG_FLAG=--enable-debug
 fi
+if [[ "$6" == "no_cuda_aware" ]]
+then
+    echo "disabling --enable-gpu-aware-mpi"
+    HYPRE_ENABLE_GPU_AWARE_MPI=""
+    echo $HYPRE_ENABLE_GPU_AWARE_MPI
+fi
+
 make clean
-./configure --with-MPI --with-openmp --enable-gpu-aware-mpi --enable-mixedint --with-MPI-include=$MPI_ROOT_PATH/include --with-MPI-lib-dirs=$MPI_ROOT_PATH/lib --enable-cuda-streams --with-cuda-home=$CUDA_ROOT_PATH --with-gpu-arch="$CUDA_ARCH_LIST" --enable-unified-memory $HYPRE_PRECISION_FLAG $HYPRE_DEBUG_FLAG
+./configure --with-MPI --enable-mixedint --with-MPI-include=$MPI_ROOT_PATH/include --with-MPI-lib-dirs=$MPI_ROOT_PATH/lib --enable-cuda-streams --with-cuda-home=$CUDA_ROOT_PATH --with-gpu-arch="$CUDA_ARCH_LIST" --enable-unified-memory $HYPRE_PRECISION_FLAG $HYPRE_DEBUG_FLAG --with-openmp $HYPRE_ENABLE_GPU_AWARE_MPI 
 make -j
 cd test
 make ij -j
